82c36f4da8cbd67c3f9677ea001a8c52622cbc1c
elisiomualumene <elisiomualumene@gmail.com>
2023-08-11 22:32:39 +0100

    feat: right sidebar

---
+++ src/layout/rightSidebar/index.tsx
@@ -0,0 +1,7 @@
+export const RightSidebar = () => {
+    return (
+        <nav className="fixed w-[250px] bg-white right-0 top-0 h-full">
+            <h1>Right Sidebar</h1>
+        </nav>
+    )
+}
