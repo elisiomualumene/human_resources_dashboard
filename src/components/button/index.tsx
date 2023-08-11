326fc230ca3ec5206f911739503982c3079785e6
elisiomualumene <elisiomualumene@gmail.com>
2023-08-11 22:31:47 +0100

    feat: button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,17 @@
+import {ComponentProps, ReactNode} from "react"
+
+interface IButtonProps extends ComponentProps<'button'> {
+    icon: ReactNode,
+    title: string
+}
+
+export const Button = ({icon, title, ...rest}: IButtonProps) => {
+    return (
+        <button 
+            className=" flex flex-row text-zinc-300 hover:text-white hover:font-bold text-sm items-center hover:bg-blue-500 px-2 py-[13px] gap-x-2 w-full rounded"
+            {...rest}>
+            {icon && icon}
+            <p>{title}</p>
+        </button>
+    )
+}
