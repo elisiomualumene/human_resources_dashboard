bd0333a207931ab3f924530aba6522af90764379
elisiomualumene <elisiomualumene@gmail.com>
2023-08-12 01:25:45 +0100

    feat: card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,18 @@
+export type ICardProps = {
+    title: string;
+    range: number;
+    percentage: number;
+}
+
+export const Card = ({percentage,range,title}: ICardProps) => {
+
+    return (
+        <aside className="flex flex-row justify-between items-center p-2 bg-white rounded">
+            <div>
+                <p className="text-xs text-zinc-300 mb-2">{title}</p>
+                <p className="text-lg font-bold">{range}</p>
+                <p className="text-xs text-orange-300 my-2">+{percentage}% Inc</p>
+            </div>
+        </aside>
+    )
+}
