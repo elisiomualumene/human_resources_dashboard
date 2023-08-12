62fbdf82a4d57dfe1665920406ed999708c35af4
elisiomualumene <elisiomualumene@gmail.com>
2023-08-12 01:25:32 +0100

    refactor: refacored button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -8,7 +8,7 @@ interface IButtonProps extends ComponentProps<'button'> {
 export const Button = ({icon, title, ...rest}: IButtonProps) => {
     return (
         <button 
-            className=" flex flex-row text-zinc-300 hover:text-white hover:font-bold text-sm items-center hover:bg-blue-500 px-2 py-[13px] gap-x-2 w-full rounded"
+            className=" flex flex-row text-zinc-300 hover:text-white hover:font-bold text-sm items-center hover:bg-blue-500 px-2 py-[13px] gap-x-2 w-full rounded transition ease-in-out delay-50"
             {...rest}>
             {icon && icon}
             <p>{title}</p>
