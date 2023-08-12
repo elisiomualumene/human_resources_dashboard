11fdd85a28840c849a86c8fab590d01fcc75cba0
elisiomualumene <elisiomualumene@gmail.com>
2023-08-12 03:08:14 +0100

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return (
+        <main className="text-center pt-16">
+            <h1 className="text-center text-2xl pt-16">Página não encontrada!</h1>
+            <p className="text-6xl mt-2">404</p>
+        </main>
+    )
+}
